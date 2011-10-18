9c1bc7e7448eb93019483eab34a2888658aea9c8
Nicolas Cagnard <nicolas.cagnard@gmail.com>
2011-10-18 00:00:00 +0000

    version 1.1

--- R/annot.R
+++ R/annot.R
@@ -1,56 +1,72 @@
+#annot_file=""; data_dir=""; res_path=""; display=TRUE
 annot <-
-function(n=FALSE, path="")
+function(annot_file="", data_dir="", res_path="", display=TRUE)
 {  
-  annotlist<-function(annot, list_id, path, nom, n)
+  annotlist<-function(annot, list_id, res_path, display)
   {
-    if(n)  list_id = read.table(file=list_id, header=TRUE, sep="\t")
-    rownames(list_id) = list_id[,1]
-    #liste seulement d'ID => 1 colonne
-    if(ncol(list_id)==1)  res = annot[duplicated(c(rownames(list_id), rownames(annot)))[nrow(list_id):nrow(annot)],]
-    #liste seulement d'ID + datas => >=2 colonnes 
-    if(ncol(list_id)>=2)  res = cbind(annot[duplicated(c(rownames(list_id), rownames(annot)))[(nrow(list_id)+1):(nrow(annot)+nrow(list_id))],], list_id[,2:ncol(list_id)])  
+    liste = read.table(file=list_id, header=TRUE, sep="\t")
+    res = annot[duplicated(c(liste[,1], annot[,1]))[(nrow(liste)+1):(nrow(annot)+nrow(liste))],]
+    rest = cbind(liste[order(liste[,1]),], res[order(res[,1]),])
+
+    if(nrow(res)!=nrow(liste))
+    {
+      reste = liste[!duplicated(c(res[,1], liste[,1]))[(nrow(res)+1):(nrow(res)+nrow(liste))],]
+      write.table(res, row.names=FALSE, file = paste(res_path, "/Rest_", substr(basename(list_id), 0, (nchar(basename(list_id))-4)), ".txt", sep=""), sep="\t")
+    }
+    
     if(nrow(res)!=0)
     {
-      write.table(res, file = paste(getwd(), "/", nom, "_annot.txt", sep=""), sep="\t")
-    }else{
-      write("No annotation found.", file="")
+      write.table(res, row.names=FALSE, file = paste(res_path, "/Annot_", substr(basename(list_id), 0, (nchar(basename(list_id))-4)), ".txt", sep=""), sep="\t")
     }
-    return(TRUE)
+    if(display)  write(paste("\t\t", substr(basename(list_id), 0, (nchar(basename(list_id))-4)), ": Annotations=", (round(nrow(res)/nrow(liste), digits=2)*100), "%", sep=""), file="")
   }
   
-  write("\t########################################################", file="")
-  write("\t#                                                      #", file="")
-  write("\t#                     Annot (v0.1)                     #", file="")
-  write("\t#                                                      #", file="")
-  write("\t########################################################\n", file="")
-  write("\t[Run man() for quick help]\n", file="")
+  if(display)  
+  {
+    write("\t########################################################", file="")
+    write("\t#                                                      #", file="")
+    write("\t#                     Annot (v1.1)                     #", file="")
+    write("\t#                                                      #", file="")
+    write("\t########################################################", file="")
+    write("\t[Run man.annot() for quick help]", file="")
+    write("\tPrototype: annot(annot_file=\"\", data_dir=\"\")\n", file="")
+  }
+  if(res_path=="")
+  {
+    res_path = paste(getwd(), "/AnnotLists_", format(Sys.time(), "(%H-%M-%S)_%a_%d_%b_%Y"), sep="")
+    dir.create(res_path)
+  }
   
-  write("Annotations file", file="")
+  if(display)  write("\tAnnotations file", file="")
   flush.console()
-  if(Sys.info()["sysname"]=="Windows") #windows
+  if(annot_file!="")
   {
-    annot = read.table(file=file.choose(), header=TRUE, row.names=1, sep="\t")
+    annot = read.table(file=annot_file, header=TRUE, sep="\t")
   }else{
-    capabilities( )["tcktk"]  #linux
-    require(tcltk)
-    annot = read.table(file=tk_choose.files(), header=TRUE, row.names=1, sep="\t")
-  }
-  
-  if(!n)  #One list annotation
-  {
-    write("List to annotate", file="")
-    flush.console()
-    if(Sys.info()["sysname"]=="Windows")  list_id = read.table(file=file.choose(), header=TRUE, sep="\t")
-    if(Sys.info()["sysname"]!="Windows")  list_id = read.table(file=tk_choose.files(), header=TRUE, sep="\t")
-    annotlist(annot, list_id, path=getwd(), nom="list")
-  }else{  #Multiple lists annotation
-    write("Lists folder", file="")
-    flush.console()
-    if(Sys.info()["sysname"]=="Windows")  listes = as.matrix(list.files(path = choose.dir(), full.names = TRUE))
-    if(Sys.info()["sysname"]!="Windows")  listes = as.matrix(list.files(path = tk_choose.dir(), full.names = TRUE))
-    apply(listes, 1, function(x) annotlist(annot, x, path=getwd(), nom=substr(basename(x), 0, (nchar(basename(x))-4)), n))
+    if(Sys.info()["sysname"]=="Windows")
+    {
+      annot = read.table(file=file.choose(), header=TRUE, sep="\t")
+    }else{
+      require(tcltk)
+      annot = read.table(file=tk_choose.files(), header=TRUE, sep="\t")
+    }
   }
 
-  write(paste("Resulting files have been placed: ", getwd(), sep=""), file="")
+  if(display)  write("\tLists folder\n", file="")
+  flush.console()
+  if(data_dir!="")
+  {
+    listes = as.matrix(list.files(path = data_dir, full.names = TRUE)) 
+  }else{
+    if(Sys.info()["sysname"]=="Windows")
+    {
+      listes = as.matrix(list.files(path = choose.dir(), full.names = TRUE))
+    }else{
+      listes = as.matrix(list.files(path = tk_choose.dir(), full.names = TRUE))
+    }
+  }
+  apply(listes, 1, function(x) annotlist(annot, x, res_path, display))
+
+  if(display)  write(paste("\n\tResulting files have been placed @ ", res_path, sep=""), file="")
 }
 
--- R/man.R
+++ R/man.R
@@ -1,12 +1,16 @@
-man <-
+man.annot <-
 function()
 {
   write("\t\t#################################", file="")
   write("\t\t#       Annot Quick Start       #", file="")
   write("\t\t#################################", file="")
-  write("\n\t# - Prototype: annot(n=TRUE/FALSE, path)", file="")
-  write("\t# - n=FALSE: One list annotation.\t\tannot()", file="")
-  write("\t# - n=TRUE: Multiple lists annotation.\t\tannot(n=TRUE)", file="")
-  write("\t# - path: path of the annotated results.", file="")
+  write("\n\t# The lists of IDs must be in text tabulated format and placed alone in a folder.", file="")
+  write("\n\t# The annotation file must be in text tabulated format and placed outside of the precedent folder.", file="")
+  write("\n\t# The common identifiers must be placed in the first column in the lists and in the annotations file.", file="")
+  write("\n\t# - Prototype: annot(annot_file=\"\", data_dir=\"\")", file="")
+  write("\t# - annot_file: path for the annotations file.", file="")
+  write("\t# - data_dir: path of the folder where are placed the lists.", file="")
+  write("\t# - res_path: path of the resulting annotated lists.", file="")
+  write("\t# - display [TRUE/FALSE]: if informations could be displayed during the process.", file="")
 }
 
